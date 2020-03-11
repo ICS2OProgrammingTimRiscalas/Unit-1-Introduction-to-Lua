d58e7e1be64d59dc3ecc993d284e855033474fad
Tim Riscalas <timothy.riscalas@ocsbstudent.ca>
2020-03-11 15:40:49 -0400

    updated NumText

--- MathFunTim/main.lua
+++ MathFunTim/main.lua
@@ -1,7 +1,9 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: MathFun
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program shows a math game with random addition, 
+-- subtraction, multiplication, and division questions.
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- 
--- NumTextFieldTim/main.lua
+++ NumTextFieldTim/main.lua
@@ -23,6 +23,10 @@ local randomNumber2
 local userAnswer
 local correctAnswer
 local incorrectAnswer
+local points = 0
+local youWin
+local incorrectPoints = 0
+local gameOver
 
 -------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
@@ -50,6 +54,14 @@ local function HideIncorrect()
 	AskQuestion()
 end
 
+local function HideYouWin()
+	youWin.isVisible = false
+end
+
+local function HideGameOver()
+	gameOver.isVisible = false
+end
+
 local function NumericFieldListener( event)
 
 	-- user begins editing "numericField"
@@ -67,16 +79,57 @@ local function NumericFieldListener( event)
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
 			incorrectObject.isVisible = false
+			youWin.isVisible = false
 			timer.performWithDelay(1500, HideCorrect)
 
+			-- give a point if user gets the correct answer
+			points = points + 1
+
+			-- update it in the display object
+			pointsText.text = "Points = " .. points
+
+			if (points >= 5) then
+				youWin.isVisible = true
+				correctObject.isVisible = false
+
+				-- clear points to 0 after winning
+				points = 0
+				incorrectPoints = 0
+				pointsText.text = "Points = " .. points
+				incorrectPointsText.text = "Times Wrong = " .. incorrectPoints 
+				timer.performWithDelay(3000, HideYouWin)
+				timer.performWithDelay(3500, AskQuestion)
+			end
+
+		-- inside the else means they got it wrong
 		else
 			incorrectObject.isVisible = true
-			correctObject.isVisible = false
-			timer.performWithDelay(2500, HideIncorrect)
 
+			-- give a point to incorrectPoints if user gets the wrong answer
+			incorrectPoints = incorrectPoints + 1
 
+			-- update it in the display object
+			incorrectPointsText.text = "Times Wrong = " .. incorrectPoints
+			timer.performWithDelay(2000, HideIncorrect)
+
+			-- if the user gets the wrong answer 3 times:
+			if (incorrectPoints == 3) then
+				gameOver.isVisible = true
+				incorrectObject.isVisible = false
+
+				-- clear points to 0 after losing
+				incorrectPoints = 0
+				points = 0
+				incorrectPointsText.text = "Times Wrong = " .. incorrectPoints 
+				pointsText.text = "Points = " .. points
+				timer.performWithDelay(3000, HideGameOver)
+				timer.performWithDelay(3500, AskQuestion)
+			end
 
 		end
+		
+
+
 		event.target.text = ""
 	end
 end
@@ -88,7 +141,7 @@ end
 -- displays a question and sets the color
 questionObject = display.newText("", display.contentWidth/3, display.contentHeight * 2/3, nil, 60)
 questionObject:setTextColor(60/255, 185/255, 51/255)
---questionObject.isVisible = true
+questionObject.isVisible = true
 
 -- create the correct text object and make it invisible
 correctObject = display.newText("Correct!", display.contentWidth/1.4, display.contentHeight * 2/3, nil, 50)
@@ -104,7 +157,23 @@ incorrectObject.isVisible = false
 numericTextFields = native.newTextField( display.contentWidth/2, display.contentHeight * 2/3, 200, 100)
 numericTextFields.inputType = "number"
 
+-- display the amount of points as a text object
+pointsText = display.newText("Points = " .. points, display.contentWidth/1.2, display.contentHeight/7, nil, 50)
+
+-- display the amount of incorrect points as a text object
+incorrectPointsText = display.newText("Times Wrong = " .. incorrectPoints, display.contentWidth/1.32, display.contentHeight/4.5, nil, 50)
+
 -- add the event listener for the numeric field
 numericTextFields:addEventListener("userInput", NumericFieldListener)
+
+-- create the youWin text object and make it visible
+youWin = display.newText("YOU WIN!", display.contentWidth/2, display.contentHeight/3, nil, 75)
+youWin:setTextColor(240/255, 92/255, 28/255)
+youWin.isVisible = false
+
+-- create the gameOver text object and make it visible
+gameOver = display.newText("GAME OVER", display.contentWidth/2, display.contentHeight/2, nil, 75)
+gameOver:setTextColor(198/255, 13/255, 19/255)
+gameOver.isVisible = false
 
 AskQuestion()
